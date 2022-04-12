bdc17aa25ef11f2560a1fcecdca6f22975340f91
vanderweck <simon.vanderweck@consultens.com>
2022-04-12 15:02:17 +0200

    Upload redesign_v5

--- Working/FlaskWebsite/flask3.py
+++ Working/FlaskWebsite/flask3.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 # from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -23,8 +24,14 @@ def upload():
     return render_template('upload.html')
 
 
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
-
-print(app.config)
