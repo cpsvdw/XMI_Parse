651105cede46b7bac976004b7a79d9b8792d08dd
vanderweck <simon.vanderweck@consultens.com>
2022-04-23 12:48:58 +0200

    beautifulsoup_v1

---
+++ Not Working/XMI Parse/files/beautiful soup.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import re
+
+with open("C:\Users/vanderweck\OneDrive - consultens Professional Services GmbH/Dokumente Simon/99_Masterarbeit/80_Demonstrator/04_Modelle/XMI/ID4.xmi") as ma:
+    soup = BeautifulSoup(ma)
+
+soup = BeautifulSoup(markup, "xml")
+
+file = open(
+    "C:\Users/vanderweck\OneDrive - consultens Professional Services GmbH/Dokumente Simon/99_Masterarbeit/80_Demonstrator/04_Modelle/XMI/ID4.xmi",
+    "r")
+contents = file.read()
+
+soup = BeautifulSoup(contents, "lxml")
+text = str(soup.findAll(text=True)).replace("\\n", " ")
+
+fehlerID = soup.find("TAG_01_Fehler_ID")
+fehlerbeschreibung = soup.find("TAG_02_Fehlerbeschreibung")
+
+Fehlermeldung = []
+
+while True
+   try:
